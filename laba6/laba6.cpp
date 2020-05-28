af67e00442a2779a0a8603e8e80cde6a06ff59b2
vikchik232 <vikidobryden@gmail.com>
2020-05-28 23:25:50 +0300

    add laba 6

---
+++ laba6/laba6.cpp
@@ -0,0 +1,102 @@
+﻿#include<iostream> 
+#include <list> 
+using namespace std;
+
+// Graph class represents a undirected graph 
+// using adjacency list representation 
+class Graph
+{
+    int V;    // No. of vertices 
+
+    // Pointer to an array containing adjacency lists 
+    list<int>* adj;
+
+    // A function used by DFS 
+    void DFSUtil(int v, bool visited[]);
+public:
+    Graph(int V);   // Constructor 
+    ~Graph();
+    void addEdge(int v, int w);
+    void connectedComponents();
+};
+
+// Method to print connected components in an 
+// undirected graph 
+void Graph::connectedComponents()
+{
+    // Mark all the vertices as not visited 
+    bool* visited = new bool[V];
+    for (int v = 0; v < V; v++)
+        visited[v] = false;
+
+    for (int v = 0; v < V; v++)
+    {
+        if (visited[v] == false)
+        {
+            // print all reachable vertices 
+            // from v 
+            DFSUtil(v, visited);
+
+            cout << "\n";
+        }
+    }
+    delete[] visited;
+}
+
+void Graph::DFSUtil(int v, bool visited[])
+{
+    // Mark the current node as visited and print it 
+    visited[v] = true;
+    cout << v << " ";
+
+    // Recur for all the vertices 
+    // adjacent to this vertex 
+    list<int>::iterator i;
+    for (i = adj[v].begin(); i != adj[v].end(); ++i)
+        if (!visited[*i])
+            DFSUtil(*i, visited);
+}
+
+Graph::Graph(int V)
+{
+    this->V = V;
+    adj = new list<int>[V];
+}
+
+Graph::~Graph()
+{
+    delete[] adj;
+}
+
+// method to add an undirected edge 
+void Graph::addEdge(int v, int w)
+{
+    adj[v].push_back(w);
+    adj[w].push_back(v);
+}
+
+// Drive program to test above 
+int main()
+{
+    // Create a graph given in the above diagram 
+    Graph g(5); // 5 vertices numbered from 0 to 4 
+    g.addEdge(1, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 4);
+
+    cout << "Following are connected components \n";
+    g.connectedComponents();
+
+    return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
